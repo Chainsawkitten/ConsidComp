e5b8e3e774be31b29e78fbb8b548d1c68dbe95c4
Albin Bernhardsson <catscratchesp@gmail.com>
2016-10-11 22:09:41 +0200

    Beginning of duplicates function.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
-// Runs the main program.
-// Second command line argument should be the name of the data file to load.
+/**
+ * @brief Checks whether the file contains any duplicate entries.
+ * Uses a flat tree structure (implemented in an array).
+ * @param filename The name of the file to check.
+ * @return Whether there were any duplicates.
+ */
+bool duplicates(const char* filename) {
+    /// @todo Implement the actual function.
+    
+    return false;
+}
+
+/**
+ * @brief Runs the main program.
+ * Second command line argument should be the name of the data file to load.
+ * @param argc The number of arguments given to the application.
+ * @param argv The argument values.
+ * @return Return code.
+ */
 int main(int argc, char** argv) {
     if (argc < 2) {
         return 1;
@@ -14,7 +31,7 @@ int main(int argc, char** argv) {
     chrono::time_point<chrono::high_resolution_clock> start = chrono::high_resolution_clock::now();;
     
     // Perform the program.
-    cout << argv[1] << endl;
+    cout << (duplicates(argv[1]) ? "Dubletter" : "Ej dubblett") << endl;
     
     // Output how long it took.
     double dt = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start).count();
